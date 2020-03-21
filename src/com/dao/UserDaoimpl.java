886e0f8f2e7e24ac5519b11f3eed604d8e54a78c
Administrator <Administrator@XTZ-20191112XRO>
2020-03-21 21:52:41 +0800

    将UserDao和UserDaoImple合并到一个包

---
+++ src/com/dao/UserDaoimpl.java
@@ -0,0 +1,46 @@
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dao.UserDao;
+import com.entity.User;
+import com.jdbcutils.UtilJDBC;
+
+/**
+ * userDao接口的实现类
+ * @author Administrator
+ *
+ */
+public class UserDaoimpl implements UserDao{
+	
+	List<User> list = new ArrayList<>();
+	
+	@Override
+	public List<User> querryAllUser() {
+		User user = new User();
+		String sql = "select id,name,age,gender from tb_test1";
+		ResultSet set;
+		try {
+			Connection connection = UtilJDBC.getConnection();
+			PreparedStatement statement = connection.prepareStatement(sql);
+			set = statement.executeQuery();
+			while(set.next()) {
+				user.setAgeInteger(set.getInt("id"));
+				user.setNameString(set.getString("name"));
+				user.setAgeInteger(set.getInt("age"));
+				user.setGenderString("gender");
+				list.add(user);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+}
