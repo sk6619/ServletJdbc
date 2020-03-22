0defd7434f0c3a9c4805bf98a62bbb644d9fd320
Administrator <Administrator@XTZ-20191112XRO>
2020-03-22 16:43:14 +0800

    删除User.jsp文件
    在servlet用流将数据打印到浏览器

--- src/com/conttroller/UserServlet.java
+++ src/com/conttroller/UserServlet.java
@@ -1,6 +1,7 @@
 package com.conttroller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,32 +11,47 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 import com.entity.User;
 import com.service.UserService;
 import com.service.UserServiceImple;
 
 @WebServlet("/user")
-public class UserServlet extends HttpServlet{
-	
+public class UserServlet extends HttpServlet {
+
 	UserService userService = new UserServiceImple();
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//设置编码
+		// 设置编码
 		req.setCharacterEncoding("utf-8");
-		resp.setCharacterEncoding("utf-8");
-		//建立一个list接收数据库返回的数据
+		resp.setContentType("text/html;charset=utf-8");
+		// 建立一个list接收数据库返回的数据
 		List<User> list = new ArrayList<>();
 		list = userService.seleUsers();
-		
+		PrintWriter outPrintWriter = resp.getWriter();
+		outPrintWriter.write("<table>");
+		outPrintWriter.write("<tr>");
+		outPrintWriter.write("<td>用户id</td>");
+		outPrintWriter.write("<td>用户名字</td>:");
+		outPrintWriter.write("<td>用户年龄</td>:");
+		outPrintWriter.write("<td>用户性别</td>:");
+		outPrintWriter.write("</tr>");
+		// 循环将查到的数据打印到浏览器
 		for (int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i).getIdsInteger());
+
+				outPrintWriter.write("<tr>");
+				outPrintWriter.write("<td>"+list.get(i).getIdsInteger()+"</td>");
+				outPrintWriter.write("<td>"+list.get(i).getNameString()+"</td>");
+				outPrintWriter.write("<td>"+list.get(i).getAgeInteger()+"</td>");
+				outPrintWriter.write("<td>"+list.get(i).getGenderString()+"</td>");
+				outPrintWriter.write("</tr>");
+
 		}
-		req.setAttribute("userlist", list);
-		req.getRequestDispatcher("/WEB-INF/user.html").forward(req, resp);
+		outPrintWriter.write("</table>");
+		outPrintWriter.flush();
+		outPrintWriter.close();
 	}
-	
+
 }
